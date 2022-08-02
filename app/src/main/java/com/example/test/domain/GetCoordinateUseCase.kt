f85f0241bbe7df4442131d3a4bac51fefc0616df
Vanilla <vetal-vetal-vv@mail.ru>
2022-08-02 19:59:19 +0300

    update domain

--- app/src/main/java/com/example/test/domain/GetCoordinateUseCase.kt
+++ app/src/main/java/com/example/test/domain/GetCoordinateUseCase.kt
@@ -2,14 +2,15 @@ package com.example.lesson_1.domain
 
 import com.example.test.data.CoordinatesRepository
 import com.example.test.data.model.UserLocationModel
+import com.example.test.utils.ResponseDataBase
 import kotlinx.coroutines.flow.Flow
 
-class GetCoordinateUseCaseImpl(private val coordinatesRepository: CoordinatesRepository) {
-    suspend fun getLastUserCoordinate(): Flow<UserLocationModel> {
+class GetCoordinateUseCaseImpl(private val coordinatesRepository: CoordinatesRepository):GetCoordinateUseCase {
+    override suspend fun getLastUserCoordinate(): Flow<ResponseDataBase<UserLocationModel>> {
         return coordinatesRepository.getLastCoordinate()
     }
 }
 
 interface GetCoordinateUseCase {
-    suspend fun getLastUserCoordinate(): Flow<UserLocationModel>
+    suspend fun getLastUserCoordinate(): Flow<ResponseDataBase<UserLocationModel>>
 }
--- app/src/main/java/com/example/test/domain/GetPointsUseCase.kt
+++ app/src/main/java/com/example/test/domain/GetPointsUseCase.kt
@@ -2,16 +2,19 @@ package com.example.lesson_1.domain
 
 import androidx.lifecycle.LiveData
 import com.example.test.data.CoordinatesRepository
+import com.example.test.data.model.UserLocationModel
 import com.example.test.data.model.UserPointModel
+import com.example.test.data.repository.UserPointsRepo
+import com.example.test.utils.ResponseDataBase
 import kotlinx.coroutines.flow.Flow
 
-class GetPointsUseCaseImpl (private val coordinatesRepository: CoordinatesRepository):GetPointsUseCase{
-    override suspend fun getUserPoints(): Flow<List<UserPointModel>> {
-        TODO("Not yet implemented")
+class GetPointsUseCaseImpl (private val userPointsRepo: UserPointsRepo):GetPointsUseCase{
+    override suspend fun getUserPoints(): Flow<ResponseDataBase<UserPointModel>> {
+        return userPointsRepo.getUserPoints()
     }
 
 
 }
 interface GetPointsUseCase{
-    suspend fun getUserPoints(): Flow<List<UserPointModel>>
+    suspend fun getUserPoints(): Flow<ResponseDataBase<UserPointModel>>
 }
